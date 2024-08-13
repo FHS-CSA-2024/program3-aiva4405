cfd89923af5c03fb489d317e7ad6f2c8832a8124
aiva4405 <58538705+aiva4405@users.noreply.github.com>
2024-08-13 09:36:30 -0700

    Update Program3.java
    finished requirements for program 3

--- Program3.java
+++ Program3.java
@@ -3,12 +3,50 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-
+ public class Program3 {
+     public static void main(String[] args) {
+         // Create input scanner
+         Scanner scanText = new Scanner(System.in);
+         
+         // Make variables
+         int length = 0;
+         int width = 0;
+         int area = 0;
+         int perimeter = 0;
+         
+         // Tell user what to input
+         System.out.println("Enter the length of the rectangle: "); 
+         
+         // Store input
+         length = scanText.nextInt();
+         
+         // Tell user what to input
+         System.out.println("Enter the width of the rectangle: ");
+         
+         // Store input
+         width = scanText.nextInt();
+         
+         // Calculate values
+         area = length * width;
+         perimeter = (2 * length) + (2 * width);
+         
+         // Report values back to user
+         System.out.println("The rectangle's area is " + area);
+         System.out.println("The rectangle's perimeter is " + perimeter);
+         
+     }
+ }
 
 
 
 //Paste console output below:
 /*
 
+Enter the length of the rectangle: 
+123
+Enter the width of the rectangle: 
+50
+The rectangle's area is 6150
+The rectangle's perimeter is 346
 
 */
